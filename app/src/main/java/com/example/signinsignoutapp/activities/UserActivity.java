4dfd98db96373400648541a9e7875a623e87b817
rysealex <aryse54@gmail.com>
2024-11-26 17:50:28 -0600

    Added functionality to the UserActivity class. Need to fix a small bug for displaying the user first name.

--- app/src/main/java/com/example/signinsignoutapp/activities/UserActivity.java
+++ app/src/main/java/com/example/signinsignoutapp/activities/UserActivity.java
@@ -11,8 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.signinsignoutapp.R;
+import com.example.signinsignoutapp.adapters.UsersAdapter;
 import com.example.signinsignoutapp.databinding.ActivityUserBinding;
+import com.example.signinsignoutapp.models.User;
+import com.example.signinsignoutapp.utilities.Constants;
 import com.example.signinsignoutapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -23,8 +32,10 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
         setListeners();
+        getUsers();
     }
 
     private void setListeners() {
@@ -32,11 +43,42 @@ public class UserActivity extends AppCompatActivity {
     }
 
     private void getUsers() {
-
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS).get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        List<User> users  = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_FIRST_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            user.id = queryDocumentSnapshot.getId();
+                            users.add(user);
+                        }
+                        if (users.size() > 0) {
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.userRecyclerView.setAdapter(usersAdapter);
+                            binding.userRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
     }
 
-    private void showToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s","No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
     }
 
     private void loading(Boolean isLoading) {
--- app/src/main/java/com/example/signinsignoutapp/utilities/Constants.java
+++ app/src/main/java/com/example/signinsignoutapp/utilities/Constants.java
@@ -13,6 +13,5 @@ public class Constants {
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
     public static final String KEY_PREFERENCE_NAME = "signInSignOutPreference";
     public static final String KEY_IMAGE = "image";
-
     public static final String KEY_FCM_TOKEN = "fcmToken";
 }
