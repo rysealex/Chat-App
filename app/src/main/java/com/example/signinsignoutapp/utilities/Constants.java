172af9fbf268bc939c9cb4cd0d9c2b643bea5ac7
rysealex <aryse54@gmail.com>
2024-12-01 17:06:57 -0600

    Added functionality to the chat feature.

--- app/src/main/java/com/example/signinsignoutapp/utilities/Constants.java
+++ app/src/main/java/com/example/signinsignoutapp/utilities/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final String KEY_PASSWORD = "password";
     public static final String KEY_USER_ID = "userid";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
-    public static final String KEY_PREFERENCE_NAME = "signInSignOutPreference";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_FCM_TOKEN = "fcmToken";
     public static final String KEY_USER = "user";
